43fe8bb3566106b6cea8ae7f352c1a866b7a4ca2
Enzo Sanchez <enzo.sanchez@epfedu.fr>
2024-11-20 11:56:18 +0100

    Aujourd'hui j'ai créée les classes cellule, GrilleDeJeu et partie. J'ai commençé à initialiser la grille du démineur puis j'ai  défini les cellules comme placerBombe

--- miniprojet_duo/src/miniprojet_duo_SANCHEZ/Miniprojet_duo.java
+++ miniprojet_duo/src/miniprojet_duo_SANCHEZ/Miniprojet_duo.java
@@ -9,12 +9,19 @@ package miniprojet_duo_SANCHEZ;
  * @author enzos
  */
 public class Miniprojet_duo {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Démarrage du jeu Super Démineur !");
+
+        // Étape 1 : Initialisation de la partie
+        int nbLignes = 5;      // Par exemple, 5 lignes
+        int nbColonnes = 5;    // Par exemple, 5 colonnes
+        int nbBombes = 5;      // Par exemple, 5 bombes
+
+        // Création d'une nouvelle partie avec les paramètres
+        Partie partie = new Partie(nbLignes, nbColonnes, nbBombes);
+
+        // Étape 2 : Démarrage de la partie
+        partie.demarrerPartie();
     }
-    
 }
+
